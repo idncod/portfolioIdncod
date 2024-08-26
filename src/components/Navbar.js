d21cc85d1670b67e33ff045a530ce70902612af9
idncod <idncod@protonmail.com>
2024-08-26 11:38:55 +0100

    add framer motion animation to navbar & hover state & active state & resolve hydration error by adding the ClientOnly.js component specifically for framer motion elements

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,27 +1,99 @@
-import React from 'react'
-import Link from 'next/link'
+'use client';
+
+import React, { useEffect, useState } from 'react';
+import Link from 'next/link';
+import { motion } from 'framer-motion';
 import styles from './Navbar.module.scss';
+import ClientOnly from '../components/ClientOnly';
 
 const Navbar = () => {
-    <nav className={`${styles.navbar}`}>
+    const [isMounted, setIsMounted] = useState(false);
+
+    useEffect(() => {
+        setIsMounted(true);
+    }, []);
+
+    const underlineVariants = {
+        initial: { width: '0%' },
+        hover: { width: '100%', transition: { duration: 0.3 } },
+    };
+
+    const wiggleVariants = {
+        animate: {
+            rotate: [0, -5, 5, -5, 5, 0],
+            transition: { duration: 0.6, ease: 'easeInOut', repeat: 1 },
+        },
+    };
+
+    return (
+        <nav className={styles.navbar}>
             <ul className={styles.navList}>
-            <li className={`${styles.li}`}>
-                <Link href="/blog">
-                    Blog
-                </Link>
-            </li>
-            <li className={`${styles.li}`}>
-                <Link href="/work">
-                    Work
-                </Link>
-            </li>
-            <li className={`${styles.li}`}>
-                <Link href="/contact" className={`${styles.li}`}>
-                <button>Contact</button>
-                </Link></li>
+                <li className={styles.li}>
+                    <Link href="/blog" passHref>
+                        <ClientOnly>
+                        <motion.div
+                            className={styles.linkWrapper}
+                            whileHover="hover"
+                            initial="initial"
+                            key={isMounted ? 'mounted' : 'unmounted'}
+                        >
+                            <motion.a
+                                className={styles.link}
+                                animate={isMounted ? 'animate' : false}
+                                variants={wiggleVariants}
+                            >
+                                Blog
+                            </motion.a>
+                            <motion.span className={styles.underline} variants={underlineVariants} />
+                        </motion.div>
+                        </ClientOnly>
+                    </Link>
+                </li>
+                <li className={styles.li}>
+                    <Link href="/work" passHref>
+                        <ClientOnly>
+                        <motion.div
+                            className={styles.linkWrapper}
+                            whileHover="hover"
+                            initial="initial"
+                            key={isMounted ? 'mounted' : 'unmounted'}
+                        >
+                            <motion.a
+                                className={styles.link}
+                                animate={isMounted ? 'animate' : false}
+                                variants={wiggleVariants}
+                            >
+                                Work
+                            </motion.a>
+                            <motion.span className={styles.underline} variants={underlineVariants} />
+                        </motion.div>
+                        </ClientOnly>
+                    </Link>
+                </li>
+                <li className={styles.li}>
+                    <Link href="/contact" passHref>
+                        <ClientOnly>
+                        <motion.div
+                            className={styles.linkWrapper}
+                            whileHover="hover"
+                            initial="initial"
+                            key={isMounted ? 'mounted' : 'unmounted'}
+                        >
+                            <motion.a
+                                className={styles.link}
+                                animate={isMounted ? 'animate' : false}
+                                variants={wiggleVariants}
+                            >
+                                Contact
+                            </motion.a>
+                            <motion.span className={styles.underline} variants={underlineVariants} />
+                        </motion.div>
+                        </ClientOnly>
+                    </Link>
+                </li>
             </ul>
         </nav>
-}
-
+    );
+};
 
 export default Navbar;
