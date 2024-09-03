61d31d8f9b655e52532282802a986518d0f7a918
idncod <idncod@protonmail.com>
2024-09-03 14:30:17 +0100

    firebase.js now using modular SDK

--- src/firebase.js
+++ src/firebase.js
@@ -1,23 +1,19 @@
-import { initializeApp } from "firebase/app"
-import { getFirestore } from "firebase/firestore"
-import firebase from 'firebase/app';
-import 'firebase/firestore';
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
 
-//  require('dotenv').config();
-
+// Ensure you have loaded environment variables correctly
 const firebaseConfig = {
-  apiKey:process.env.FIREBASE_API_KEY,
-  authDomain:process.env.FIREBASE_AUTH_DOMAIN,
-  projectId:process.env.FIREBASE_PROJECT_ID,
-  storageBucket:process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId:process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId:process.env.FIREBASE_APP_ID
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 };
 
-
 // Initialize Firebase
-
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-export { db, app, firebase};
+// Export only the necessary objects
+export { db, app };
