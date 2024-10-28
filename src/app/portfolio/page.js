4f5ef5156da9704a3606c85b1eca1dd12b61a4a0
idncod <idncod@protonmail.com>
2024-10-28 11:29:08 +0000

    add portfolio temp page

---
+++ src/app/portfolio/page.js
@@ -0,0 +1,108 @@
+/* eslint-disable no-unused-vars */
+'use client';
+import Image from "next/image";
+import Pic1 from '../../../public/images/1.jpg'
+import Pic2 from '../../../public/images/1.jpeg'
+import { useScroll, useTransform, motion } from "framer-motion";
+import { useEffect, useRef } from "react";
+import Lenis from 'lenis';
+import styles from '../page.module.scss';
+import Link from 'next/link';
+
+export default function Home() {
+
+  const container = useRef();
+  const { scrollYProgress } = useScroll({
+    target: container,
+    offset: ["start start", "end end"]
+  }) 
+
+  useEffect( () => {
+    const lenis = new Lenis()
+
+    function raf(time) {
+      lenis.raf(time)
+      requestAnimationFrame(raf)
+    }
+
+    requestAnimationFrame(raf)
+  }, [])
+
+  return (
+    <main ref={container} className={styles.container}>
+      <Section1 scrollYProgress={scrollYProgress}/>
+      <Section2 scrollYProgress={scrollYProgress}/>
+    </main>
+  );
+}
+
+const Section1 = ({scrollYProgress}) => {
+
+  const scale = useTransform(scrollYProgress, [0, 1], [1, 0.8]);
+  const rotate = useTransform(scrollYProgress, [0, 1], [0, -5])
+  return (
+    <motion.div style={{scale, rotate}} className={styles.containerPosition}>
+      <p>Portfolio is being populated with fresh stuff. Please, check back in later.<br/>Try scrolling!</p>
+      <Link href="/" className={styles.goBack}>
+        <div className={styles.goBack}>
+          Go to Home
+        </div>
+      </Link>
+    </motion.div>
+  )
+}
+
+const Section2 = ({scrollYProgress}) => {
+  const scale = useTransform(scrollYProgress, [0, 1], [0.8, 1]);
+  const rotate = useTransform(scrollYProgress, [0, 1], [5, 0]);
+  const opacity = useTransform(scrollYProgress, [0, 0.2, 1], [0, 1, 1]);
+
+  return (
+    <motion.div 
+      style={{scale, rotate, opacity}} 
+      className={`${styles.sectionMotion} ${styles.sectionMotionContainer}`}
+    >
+      <Image 
+        src={Pic2}
+        alt="img"
+        layout="fill"
+        objectFit="cover"
+        objectPosition="center"
+      />
+    </motion.div>
+  );
+}
+
+
+// const Section2 = ({scrollYProgress}) => {
+
+//   const scale = useTransform(scrollYProgress, [0, 1], [0.8, 1]);
+//   const rotate = useTransform(scrollYProgress, [0, 1], [5, 0])
+
+//   return (
+//     <motion.div style={{scale, rotate}} className={styles.sectionMotion}>
+//       <Image 
+//         src={Pic2}
+//         alt="img"
+//         placeholder="blur"
+//         fill
+//       />
+//     </motion.div>
+//   )
+// }
+
+// const About = () => {
+
+
+//   return (
+//     <div className="about-page">
+//       <section>
+//         <div>WHAT SETS ME APART?</div>
+//       </section>
+//     </div>
+
+//   );
+// };
+
+// export default About;
+
